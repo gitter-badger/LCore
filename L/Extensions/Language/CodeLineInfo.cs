f6af25900802e182c293a3917040cd51ee493ade
Code Singularity <codesingularity@outlook.com>
2016-08-31 21:11:02 -1000

    fixes for method comment tracking

---
+++ L/Extensions/Language/CodeLineInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using JetBrains.Annotations;
+using LCore.Interfaces;
+// ReSharper disable InconsistentNaming
+
+namespace LCore.Extensions
+    {
+
+    public class CodeLineInfo
+        {
+        public string FilePath { get; set; }
+        public uint LineNumber { get; set; }
+        public string LineText { get; set; }
+        }
+    }
