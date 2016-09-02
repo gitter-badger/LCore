b6d461ea15bbda68ef01eee6d22072d363eb0b40
BenHamel <BenHamel@LAPTOP-CB4EQPB3>
2016-09-01 16:20:51 -1000

    update for generator

--- L Tests/LCoreMarkdownGenerator.cs
+++ L Tests/LCoreMarkdownGenerator.cs
@@ -11,48 +11,50 @@ namespace LCore.Tests
     /// <summary>
     /// Generates markdown for the LUnit project
     /// </summary>
-    public class LCoreMarkdownGenerator : MarkdownGenerator_L
+    public class LCoreMarkdownGenerator : SolutionMarkdownGenerator_L
         {
         /// <summary>
         /// Override this member to specify the assemblies to generae documentation.
         /// </summary>
-        public override Assembly[] DocumentAssemblies => new[] { Assembly.GetAssembly(typeof(L)) };
+        public override Assembly[] DocumentAssemblies => new[] {Assembly.GetAssembly(typeof(L))};
 
-        public override void Home_Intro(GitHubMarkdown MD)
+        public override void Home_Intro(GeneratedDocument MD)
             {
             }
 
-        public override void HowToInstall(GitHubMarkdown MD)
+        public override void HowToInstall(GeneratedDocument MD)
             {
             MD.Line($"Add {nameof(LCore)} as a nuget package:");
-            MD.Code(new[] { $"Install-Package {nameof(LCore)}" });
+            MD.Code(new[] {$"Install-Package {nameof(LCore)}"});
             }
 
         public override List<ProjectInfo> Home_RelatedProjects
             => base.Home_RelatedProjects.Select(Project => Project.Name != nameof(LCore));
 
+        public override string RootUrl => LUnit.LUnit.Urls.GitHubRepository_LCore;
+
         /// <summary>
         /// Override this value to display a large image on top ofthe main document
         /// </summary>
-        public override string BannerImage_Large(GitHubMarkdown MD) =>
+        public override string BannerImage_Large(GeneratedDocument MD) =>
             MD.GetRelativePath($"{typeof(L).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore)}-banner-large.png");
 
         /// <summary>
         /// Override this value to display a small banner image on top of sub-documents
         /// </summary>
-        public override string BannerImage_Small(GitHubMarkdown MD) =>
+        public override string BannerImage_Small(GeneratedDocument MD) =>
             MD.GetRelativePath($"{typeof(L).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore)}-banner-small.png");
 
         /// <summary>
         /// Override this value to display a large image in the upper right corner of the main document
         /// </summary>
-        public override string LogoImage_Large(GitHubMarkdown MD) =>
+        public override string LogoImage_Large(GeneratedDocument MD) =>
             MD.GetRelativePath($"{typeof(L).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore)}-logo-small.png");
 
         /// <summary>
         /// Override this value to display a small image in the upper right corner of sub-documents
         /// </summary>
-        public override string LogoImage_Small(GitHubMarkdown MD) =>
+        public override string LogoImage_Small(GeneratedDocument MD) =>
             MD.GetRelativePath($"{typeof(L).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore)}-logo-small.png");
 
         public override bool RequireDirectLinksToAllForeignTypes => true;
